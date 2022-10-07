d8eef7baaee8c59af0e8b3441d724d5a272e902e
Jin <novhunter@gmail.com>
2022-10-06 18:10:45 -0600

    initialize repository

---
+++ numbers.py
@@ -0,0 +1,17 @@
+import random
+
+MyInteger = 1492
+SecondInteger = 2941
+MyRandomInteger = random.randrange(1492,2941)
+
+TimePassed = (MyRandomInteger - MyInteger)
+print(MyRandomInteger)
+print(TimePassed)
+
+ApplePrice = 1.12
+OrangePrice = 1.42
+BananaPriceInt = 3
+BananaPriceFloat = float(BananaPriceInt)
+
+Bill = ApplePrice + OrangePrice + BananaPriceInt
+print(Bill)
